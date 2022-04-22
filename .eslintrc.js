e6c6cbaf6695a02e98f8909f6c6cc4c6ef0c21e9
fatihcandev <facan@cisco.com>
2022-04-22 12:00:39 +0300

    install eslint prettier plugin + config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'plugin:storybook/recommended'],
+  extends: ['next/core-web-vitals', 'plugin:storybook/recommended', 'prettier'],
+  plugins: ['prettier'],
   rules: {
     'no-unused-vars': 'error',
   },
