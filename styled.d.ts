2966c4fd72a216ae1fe74d8c66e372c6ae78eb99
fatihcandev <facan@cisco.com>
2022-04-22 11:25:05 +0300

    add theme

---
+++ styled.d.ts
@@ -0,0 +1,17 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      moderateBlue: string
+      softRed: string
+      lightGrayishBlue: string
+      paleRed: string
+      darkBlue: string
+      grayishBlue: string
+      lightGray: string
+      veryLightGray: string
+      white: string
+    }
+  }
+}
