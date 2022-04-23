9fcaf1934726722cf9ba56122d648fb8fc9c77a3
fatihcandev <facan@cisco.com>
2022-04-23 15:43:00 +0300

    update _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -42,7 +42,7 @@ export default class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin="true"
+            crossOrigin="anonymous"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap"
