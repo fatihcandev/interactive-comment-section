c8bd213649b1e4c0fcefd0ca2db8998ae2464d8b
Fatih <contact@fatihcan.dev>
2022-04-24 23:31:22 +0300

    install typescript eslint plugin + parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,15 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'plugin:storybook/recommended', 'prettier'],
-  plugins: ['prettier'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    'no-unused-vars': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
     '@next/next/no-img-element': 'off',
   },
 }
