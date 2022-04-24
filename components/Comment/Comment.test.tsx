13b7435a7cf39b2a27553a6e6862e4082d64a704
Fatih <contact@fatihcan.dev>
2022-04-24 21:50:57 +0300

    add empty test cases

---
+++ components/Comment/Comment.test.tsx
@@ -0,0 +1,19 @@
+describe(('tests') => {
+  it('should upvote the comment when the upvote button is clicked', () => {})
+
+  it('should downvote the comment when the downvote button is clicked', () => {})
+
+  it('should open the dialog when the delete button is clicked', () => {})
+
+  it('should close the dialog when "No, Cancel" button is clicked', () => {})
+
+  it('should close the dialog and delete the comment when "Yes, Delete" button is clicked', () => {})
+
+  it('should toggle the comment into the editing state when the edit button is clicked', () => {})
+
+  it('should render the Reply component under the comment thats being replied to when the reply button is clicked', () => {})
+
+  it('should show an alert if the input is empty when the send button is clicked', () => {})
+
+  it('should create a new comment when the send button is clicked', () => {})
+})
