d3b0b21d08c954dc69ab5db340a49b45164051fc
Fatih <contact@fatihcan.dev>
2022-04-24 16:50:31 +0300

    create Icon components from svg files and remove svg files

---
+++ components/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as Delete } from './Delete'
+export { default as Edit } from './Edit'
+export { default as Minus } from './Minus'
+export { default as Plus } from './Plus'
+export { default as Reply } from './Reply'
