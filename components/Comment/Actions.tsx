c5295d62a70c8b78396ea3af97eb7a2a9f19e8a5
Fatih <contact@fatihcan.dev>
2022-04-24 17:44:07 +0300

    update Actions

--- components/Comment/Actions.tsx
+++ components/Comment/Actions.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { IconButton } from 'components'
+import { Delete, Edit, Reply } from 'components/icons'
+
 type ActionsProps = {
   isCurrentUser: boolean
 }
@@ -8,9 +11,47 @@ type ActionsProps = {
 export const Actions: React.FunctionComponent<ActionsProps> = ({
   isCurrentUser,
 }) => {
-  return <Container>{isCurrentUser ? 'Delete Edit' : 'Reply'}</Container>
+  return (
+    <Container>
+      {isCurrentUser ? (
+        <DeleteEditActionContainer>
+          <StyledIconButton
+            icon={<Delete />}
+            label="Delete"
+            color="softRed"
+            onClick={() => console.log('clicked on delete')}
+          />
+          <StyledIconButton
+            icon={<Edit />}
+            label="Edit"
+            onClick={() => console.log('clicked on edit')}
+          />
+        </DeleteEditActionContainer>
+      ) : (
+        <StyledIconButton
+          icon={<Reply />}
+          label="Reply"
+          onClick={() => console.log('clicked on reply')}
+        />
+      )}
+    </Container>
+  )
 }
 
 const Container = styled.div`
   margin-left: auto;
+`
+
+const DeleteEditActionContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 24px;
+`
+
+const StyledIconButton = styled(IconButton)`
+  transition: opacity 0.2s ease;
+
+  &:hover {
+    opacity: 0.4;
+  }
 `
