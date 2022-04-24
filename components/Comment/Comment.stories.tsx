aa753cafbbb92b404b0281936bcfe77ff75b2309
Fatih <contact@fatihcan.dev>
2022-04-24 17:45:14 +0300

    add CurrentUser to Comment story

--- components/Comment/Comment.stories.tsx
+++ components/Comment/Comment.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-
 import { ComponentStory, ComponentMeta } from '@storybook/react'
+import dayjs from 'dayjs'
 
 import { mockData } from 'data'
 import { Comment, CommentProps } from './Comment'
@@ -10,11 +10,18 @@ export default {
   component: Comment,
   decorators: [
     (story: ComponentStory<typeof Comment>, context) => (
-      <div style={{ maxWidth: 500 }}>
+      <div style={{ maxWidth: 642 }}>
         {story(story.args as CommentProps, context)}
       </div>
     ),
   ],
+  argTypes: {
+    comment: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 } as ComponentMeta<typeof Comment>
 
 const Template: ComponentStory<typeof Comment> = args => <Comment {...args} />
@@ -23,4 +30,20 @@ export const Primary = Template.bind({})
 
 Primary.args = {
   comment: mockData.comments[0],
+}
+
+export const CurrentUser = Template.bind({})
+
+CurrentUser.args = {
+  comment: {
+    id: 4,
+    content:
+      "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+    createdAt: dayjs().subtract(2, 'days').toDate(),
+    score: 2,
+    user: {
+      image: '/images/image-juliusomo.png',
+      username: 'juliusomo',
+    },
+  },
 }
